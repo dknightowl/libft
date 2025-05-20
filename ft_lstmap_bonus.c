525b2da687b2f7c6ac3e0d843aed47720716a767
dknightowl <dkludge85@gmail.com>
2025-05-21 01:41:44 +0800

    fix missing call to free for new_node's content in lstmap

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dkhoo <dkhoo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 13:32:15 by dkhoo             #+#    #+#             */
-/*   Updated: 2025/05/13 15:51:07 by dkhoo            ###   ########.fr       */
+/*   Updated: 2025/05/21 01:14:18 by dkhoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
 	t_list	*new_node;
+	void	*new_content;
 
 	if (!lst || !f || !del)
 		return (NULL);
@@ -34,10 +35,12 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	new_node = NULL;
 	while (lst)
 	{
-		new_node = ft_lstnew(f(lst->content));
+		new_content = f(lst->content);
+		new_node = ft_lstnew(new_content);
 		if (!new_node)
 		{
 			ft_lstclear(&new_lst, (*del));
+			free(new_content);
 			return (NULL);
 		}
 		ft_lstadd_back(&new_lst, new_node);
